61e95100f4da4d074a5f3ef292ec4aa5a4bd629b
Dominic Baird <dominic.baird@student.hhs.school.nz>
2026-09-16 15:08:08 +1200

    made pop over fully work and form links to button in pop over

--- pop-up.js
+++ pop-up.js
@@ -1,5 +1,11 @@
+
 window.onload = function () {
-    document.getElementById('button').onclick = function () {
-        document.getElementById('modal').style.display = "none"
+    // register a function to close the modal dialogue when the close button is pressed
+    document.getElementById('close-form').onclick = function () {
+        document.getElementById('modal').style.display = "none" 
+    }
+    // register a function to open the modal dialogue when the get to know you button is pressed
+    document.getElementById('get-to-know-you').onclick = function () {
+        document.getElementById('modal').style.display = "block"
     }
 }
